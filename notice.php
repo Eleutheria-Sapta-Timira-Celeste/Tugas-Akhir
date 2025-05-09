c69fc7428afb9df9a3b735821337b9cb4e705a38
Yola Nabilah <yolanabilah30@gmail.com>
2025-05-09 18:24:42 +0700

    Ubah nama notice

--- notice.php
+++ notice.php
@@ -9,7 +9,7 @@ include 'connection/database.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Notice</title>
+    <title>Pengumuman</title>
     <script defer src="https://unpkg.com/alpinejs@3.2.3/dist/cdn.min.js"></script>
     <link rel="icon" type="image/x-icon" href="assects/images/logo2.png">
 
