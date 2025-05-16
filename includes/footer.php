20c459e3599867c7834023c67d502874110a3069
Yola Nabilah <yolanabilah30@gmail.com>
2025-05-16 23:10:16 +0700

    update kesalahan footer

--- includes/footer.php
+++ includes/footer.php
@@ -43,11 +43,9 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
     <div class="bg-[#fa9c61]">
-=======
+
     <div class="bg-[#ffe5b4]">
->>>>>>> 89a50dc11f1885dab1f26294f1ec2c08f773f18a
         <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p class="text-sm md:text-base text-gray-500 text-sm text-center sm:text-left">Copyright © 2025 - <?php echo date("Y"); ?> All rights reserved by -
                 <a href="https://technicalpashupati.edu.np" rel="noopener noreferrer" class="text-[#888] ml-1" target="_blank">SMP PGRI 371 Pondok Aren</a>
