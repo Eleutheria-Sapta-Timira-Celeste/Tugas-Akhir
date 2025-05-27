e09fc9fc823c1fe1d4eff0e75486f3890f14cd4b
Yola Nabilah <nabilahyola@gmail.com>
2025-05-27 19:16:10 +0700

    update spmb_siswa tapi blm bener

--- admin/registered_students.php
+++ admin/registered_students.php
@@ -17,7 +17,7 @@ if (isset($_POST['student_delete'])) {
     echo '<script>console.log("Form is submitting");</script>';
     $studentId = $_POST["student_id"];
 
-    mysqli_query($connection, "DELETE FROM `admission_form` WHERE id = $studentId;");
+    mysqli_query($connection, "DELETE FROM `spmb_siswa` WHERE id = $studentId;");
     echo '
         <script>
         window.location.replace("registered_students.php");
@@ -61,79 +61,81 @@ $defaultavatar = "../assects/images/defaults/defaultaltimage.jpg"
 
 
     <!-- Start block -->
-    <section class="bg-gray-50 dark:bg-[#ef6c00] p-3 sm:p-5 antialiased">
-        <div class="mx-auto max-w-screen-2xl px-4 lg:px-12">
-            <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-
-                <div class="overflow-x-auto">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="p-4">Nama Siswa</th>
-                                <th scope="col" class="p-4">Ekstrakulikuler</th>
-                                <th scope="col" class="p-4">Tanggal</th>
-                                <th scope="col" class="p-4">Terakhir diperbarui</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-
-                            <?php
-                                $fetch_all_students = "SELECT * FROM `admission_form` ORDER BY id DESC;";
-                                $students = mysqli_query($connection, $fetch_all_students);
-                                $totalStudents = mysqli_num_rows($students);
-
-                                if ($totalStudents > 0) {
-                                    while ($row = mysqli_fetch_assoc($students)) {
-                                        $student_Id = $row['id'];
-                                        echo '
-
-
-
-                                <tr class="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        <div class="flex items-center mr-3">
-                                            '.$row['full_name'].'
-                                        </div>
-                                    </th>
-                                    <td class="px-4 py-3">
-                                        <span class="bg-blue-100 text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">'.$row['admit_to'].'</span>
-                                    </td>
-                                    
-                                    <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">'.$row['registered_on'].'</td>
-                                    
-                                    <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        <div class="flex items-center space-x-4">
-                                            <button type="button" data-drawer-target="drawer-read-product-advanced'.$student_Id.'" data-drawer-show="drawer-read-product-advanced'.$student_Id.'" aria-controls="drawer-read-product-advanced'.$student_Id.'" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-2 -ml-0.5">
-                                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
-                                                </svg>
-                                                Preview
-                                            </button>
-                                            <button type="button" data-modal-target="delete-modal'.$student_Id.'" data-modal-toggle="delete-modal'.$student_Id.'" class="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 -ml-0.5" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                                </svg>
-                                                Delete
-                                            </button>
-                                        </div>
-                                    </td>
+        <section class="bg-gray-50 dark:bg-[#ef6c00] p-3 sm:p-5 antialiased">
+            <div class="mx-auto max-w-screen-2xl px-4 lg:px-12">
+                <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                    <div class="overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="p-4">Nama Siswa</th>
+                                    <th scope="col" class="p-4">Jenis Kelamin</th>
+                                    <th scope="col" class="p-4">Tanggal Pendaftaran</th>
+                                    <th scope="col" class="p-4">Terakhir diperbarui</th>
                                 </tr>
-                                ';
-                                    }
+                            </thead>
+                            <tbody>
+                <?php
+                            $fetch_all_students = "SELECT * FROM `spmb_siswa` ORDER BY id DESC;";
+                            $students = mysqli_query($connection, $fetch_all_students);
+                            $totalStudents = mysqli_num_rows($students);
+
+                            if ($totalStudents > 0) {
+                                while ($row = mysqli_fetch_assoc($students)) {
+                                    $student_Id = $row['id'];
+                                    echo '
+                                        <tr class="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex items-center mr-3">'
+                                                    . $row['nama_lengkap'] .
+                                                '</div>
+                                            </th>
+                                            <td class="px-4 py-3">
+                                                <span class="bg-blue-100 text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">'
+                                                    . ($row['jenis_kelamin'] == 'L' ? 'Laki-laki' : 'Perempuan') .
+                                                '</span>
+                                            </td>
+                                            <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">'.$row['created_at'].'</td>
+                                            <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex items-center space-x-4">
+                                                    <button type="button" 
+                                                        data-drawer-target="drawer-read-product-advanced' . $row['id'] . '" 
+                                                        data-drawer-show="drawer-read-product-advanced' . $row['id'] . '" 
+                                                        aria-controls="drawer-read-product-advanced' . $row['id'] . '" 
+                                                        class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-2 -ml-0.5">
+                                                            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
+                                                        </svg>
+                                                        Preview
+                                                    </button>
+                                                    <button type="button" 
+                                                        data-modal-target="delete-modal' . $row['id'] . '" 
+                                                        data-modal-toggle="delete-modal' . $row['id'] . '" 
+                                                        class="flex items-center text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 -ml-0.5" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                        </svg>
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ';
                                 }
-                                ?>
-                        </tbody>
-                    </table>
-                </div>
+                            }
+                    ?>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </section>
+    </div>
+</section>
+
 
 
     <?php
-        $fetch_all_students = "SELECT * FROM `admission_form` ORDER BY id DESC;";
+        $fetch_all_students = "SELECT * FROM `spmb_siswa` ORDER BY id DESC;";
         $students = mysqli_query($connection, $fetch_all_students);
         $totalStudents = mysqli_num_rows($students);
 
@@ -145,7 +147,7 @@ $defaultavatar = "../assects/images/defaults/defaultaltimage.jpg"
         <div id="drawer-read-product-advanced'.$student_Id.'" class="overflow-y-auto fixed top-0 left-0 z-50 p-4 w-full max-w-lg h-screen bg-white transition-transform -translate-x-full dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label" aria-hidden="true">
             <div>
                 <h4 id="read-drawer-label" class="mb-1.5 leading-none text-xl font-semibold text-gray-900 dark:text-white">Registered on</h4>
-                <h5 class="mb-5 text-xl font-bold text-gray-900 dark:text-white">'.$row['registered_on'].'</h5>
+                <h5 class="mb-5 text-xl font-bold text-gray-900 dark:text-white">'.$row['created_at'].'</h5>
             </div>
             <button type="button" data-drawer-dismiss="drawer-read-product-advanced'.$student_Id.'" aria-controls="drawer-read-product-advanced'.$student_Id.'" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
