f86e1fe1d10806a08ab4d2e99767754dd0c7bece
Dela Rustiana Sari <delarustianasarii@gmail.com>
2025-06-19 22:41:05 +0700

    update absen guru

--- guru/cek_login.php
+++ guru/cek_login.php
@@ -1,7 +1,11 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Cek jika belum login
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header('Location: login.php');
     exit;
 }
 ?>
