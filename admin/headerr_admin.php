cce8088fae68ace9bc89f6c3b1abc07d2bd78a92
Yola Nabilah <yolanabilah30@gmail.com>
2025-07-21 14:29:19 +0700

    update tampilan admin

---
+++ admin/headerr_admin.php
@@ -0,0 +1,13 @@
+<header class="bg-blue-700 text-white flex items-center justify-between h-16 w-full transition-all duration-300 pl-4 pr-6">
+    <div class="flex items-center gap-4">
+        <!-- Toggle Button -->
+        <button id="toggleSidebar" class="text-white text-2xl focus:outline-none">
+            <i class="fas fa-bars"></i>
+        </button>
+        <span class="font-bold text-lg">Admin DEMO</span>
+    </div>
+    <div class="flex items-center gap-4">
+        <span>Yasinta putri</span>
+        <img src="https://www.svgrepo.com/show/382106/avatar-boy.svg" class="w-8 h-8 rounded-full" alt="User">
+    </div>
+</header>
