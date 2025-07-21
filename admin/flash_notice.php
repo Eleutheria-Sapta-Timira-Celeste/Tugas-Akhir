ca1d7458ecaeaa63d1e964ffbe744a879f513cc5
Yola Nabilah <nabilahyola@gmail.com>
2025-07-21 22:24:18 +0700

    update si flash notice

--- admin/flash_notice.php
+++ admin/flash_notice.php
@@ -50,7 +50,6 @@ if (isset($_POST['update_flash_notice'])) {
 }
 ?>
 
-<!-- Mulai dari sini adalah HTML isi kontennya saja -->
 <section class="text-gray-600 body-font">
     <div class="container px-5 py-10 mx-auto">
         <div class="flex flex-col text-center w-full mb-5">
@@ -62,60 +61,66 @@ if (isset($_POST['update_flash_notice'])) {
     </div>
 </section>
 
+<!-- Mulai dari sini adalah HTML isi kontennya saja -->
 <section class="text-gray-600 body-font" id="joinUsSection">
-    <div class="container mx-auto flex md:flex-row flex-col items-start gap-10"> <!-- Tambahkan gap-10 -->
-        <!-- Preview -->
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <div class="p-6 bg-white rounded-md shadow-md max-w-sm">
-                <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900">
-                    <?= $flash_notice['title']; ?>
-                </h5>
-                <img class="rounded-t-lg" src="../<?= $flash_notice['image_url']; ?>" alt="Preview Image" />
-                <p class="my-3 text-gray-700"><?= $flash_notice['message']; ?></p>
-                <a href="#" class="inline-flex items-center py-2 px-4 text-white bg-[#e65c00] hover:bg-[#cc5200] rounded">Close</a>
+    <div class="container mx-auto px-4 py-5">
+        <div class="flex flex-col lg:flex-row items-start justify-center gap-10">
+            <!-- Preview -->
+            <div class="w-full lg:w-1/2">
+                <div class="p-6 bg-white rounded-md shadow-md h-full">
+                    <h5 class="mb-3 text-2xl font-bold tracking-tight text-gray-900 text-left">
+                        <?= $flash_notice['title']; ?>
+                    </h5>
+                    <img class="rounded-t-lg mx-auto" src="../<?= $flash_notice['image_url']; ?>" alt="Preview Image" />
+                    <p class="my-3 text-gray-700 text-justifiy"><?= $flash_notice['message']; ?></p>
+                    <div class="flex justify-left">
+                        <a href="#" class="inline-flex items-left py-2 px-4 text-white bg-[#e65c00] hover:bg-[#cc5200] rounded">Close</a>
+                    </div>
+                </div>
             </div>
-        </div>
 
-        <!-- Form -->
-        <div class="lg:flex-grow md:w-1/2 lg:pl-6 md:pl-0 text-left"> <!-- Kurangi padding kiri agar jarak lebih seragam -->
-            <section class="max-w-4xl p-6 bg-white rounded-md shadow-md mt-0 mb-10">
-                <h1 class="text-xl font-bold text-black">Kartu Sambutan</h1>
-                <form action="" method="POST" enctype="multipart/form-data">
-                    <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                        <div>
-                            <label class="text-black">Nyalakan Animasi</label>
-                            <select name="trun_flash_notice" class="w-full mt-2 p-2 border border-gray-300 rounded">
-                                <option value="1" <?= ($flash_notice['trun_flash'] == "1" ? 'selected' : ''); ?>>ON</option>
-                                <option value="0" <?= ($flash_notice['trun_flash'] == "0" ? 'selected' : ''); ?>>OFF</option>
-                            </select>
-                        </div>
+            <!-- Form -->
+            <div class="w-full lg:w-1/2">
+                <section class="p-6 bg-white rounded-md shadow-md h-full">
+                    <h1 class="text-xl font-bold text-black mb-4">Kartu Sambutan</h1>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                            <div>
+                                <label class="text-black">Nyalakan Animasi</label>
+                                <select name="trun_flash_notice" class="w-full mt-2 p-2 border border-gray-300 rounded">
+                                    <option value="1" <?= ($flash_notice['trun_flash'] == "1" ? 'selected' : ''); ?>>ON</option>
+                                    <option value="0" <?= ($flash_notice['trun_flash'] == "0" ? 'selected' : ''); ?>>OFF</option>
+                                </select>
+                            </div>
 
-                        <div>
-                            <label class="text-black">Subjek <span class="text-red-500">*</span></label>
-                            <input name="flash_title" value="<?= $flash_notice['title']; ?>" type="text" class="w-full mt-2 p-2 border border-gray-300 rounded">
-                        </div>
+                            <div>
+                                <label class="text-black">Subjek <span class="text-red-500">*</span></label>
+                                <input name="flash_title" value="<?= $flash_notice['title']; ?>" type="text" class="w-full mt-2 p-2 border border-gray-300 rounded">
+                            </div>
 
-                        <div>
-                            <label class="block text-sm font-medium text-black">Poster/Gambar</label>
-                            <div class="mt-1 border-2 border-dashed border-gray-300 p-6 text-center rounded">
-                                <input name="image_name" type="hidden" value="<?= $flash_notice['image_url']; ?>">
-                                <input type="file" accept="image/*" name="file-upload" id="file-upload" class="block w-full text-sm text-gray-600">
+                            <div class="sm:col-span-2">
+                                <label class="block text-sm font-medium text-black mb-1">Poster/Gambar</label>
+                                <div class="border-2 border-dashed border-gray-300 p-4 text-center rounded">
+                                    <input name="image_name" type="hidden" value="<?= $flash_notice['image_url']; ?>">
+                                    <input type="file" accept="image/*" name="file-upload" id="file-upload" class="block w-full text-sm text-gray-600">
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-2">
+                                <label class="text-black">Isi Pesan <span class="text-red-500">*</span></label>
+                                <textarea name="flash_message" rows="4" class="w-full mt-2 p-2 border border-gray-300 rounded"><?= $flash_notice['message']; ?></textarea>
                             </div>
                         </div>
 
-                        <div class="col-span-2">
-                            <label class="text-black">Isi Pesan <span class="text-red-500">*</span></label>
-                            <textarea name="flash_message" rows="4" class="w-full mt-2 p-2 border border-gray-300 rounded"><?= $flash_notice['message']; ?></textarea>
+                        <div class="flex justify-end mt-6">
+                            <button name="update_flash_notice" type="submit" class="px-4 py-2 bg-[#fc6941] hover:bg-[#e65c00] text-white rounded">
+                                Simpan Perubahan
+                            </button>
                         </div>
-                    </div>
-
-                    <div class="flex justify-end mt-6">
-                        <button name="update_flash_notice" type="submit" class="px-4 py-2 bg-[#fc6941] hover:bg-[#e65c00] text-white rounded">
-                            Simpan Perubahan
-                        </button>
-                    </div>
-                </form>
-            </section>
+                    </form>
+                </section>
+            </div>
         </div>
     </div>
 </section>
+
